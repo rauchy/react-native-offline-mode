9f1e63e0c4867ed221c9ab5db3fbed4dabef75c6
Omer Lachish 👾 <omer.lachish@klarna.com>
2016-04-07 09:47:27 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict'
+var React = require('react-native')
+
+var {
+  Component,
+  NetInfo,
+  StyleSheet,
+  Text,
+  View
+} = React
+
+const RequiresConnection = (WhenOnline, whenOffline) => class RequiresConnection extends Component {
+  componentWillMount () {
+    this.setState({
+      isConnected: true
+    })
+
+    NetInfo.isConnected.fetch().done((isConnected) =>
+      this.setState({
+        isConnected: isConnected
+      }))
+
+    NetInfo.isConnected.addEventListener('change', (isConnected) => {
+      this.setState({
+        isConnected: isConnected
+      })
+    })
+  }
+
+  render () {
+    if (this.state.isConnected) {
+      return <WhenOnline {...this.props} />
+    } else if (typeof whenOffline === 'function') {
+      return whenOffline()
+    } else {
+      const message = whenOffline || 'We\'re sorry, there seems to be a problem with your internet connection. The application will resume as soon as it is able to reconnect to the internet.'
+
+      return <View style={styles.container}>
+        <Text style={styles.connectionProblemMessage}>
+          {message}
+        </Text>
+      </View>
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  connectionProblemMessage: {
+    padding: 24,
+    textAlign: 'center'
+  }
+})
+
+module.exports = RequiresConnection
